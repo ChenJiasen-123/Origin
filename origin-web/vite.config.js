4e5e511a022a5cdb6eb47cd08cb4c0e43d60c56d
ChenJiasen-123 <12512805@mail.sustech.edu.cn>
2026-02-15 16:42:43 +0800

    [2]: 解决了IP硬编码的一些问题+1

--- origin-web/vite.config.js
+++ origin-web/vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // 记得引入 path 模块
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      // 设置 @ 指向 src 目录
-      '@': path.resolve(__dirname, './src')
-    }
-  },
   server: {
     host: '0.0.0.0',
     proxy: {
       '/origin': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8080',//服务器IP和端口
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/origin/, '')
       }
